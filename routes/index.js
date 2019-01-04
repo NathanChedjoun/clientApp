086fc8c103ad3b127a8314d64c422fbc3ad4ddf7
NathanChedjoun <nathanchedjoun@gmail.com>
2019-01-04 22:28:45 +0100

    Ajout Generation Pvs

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 
 var express = require('express');
 var router = express.Router();
+const axios = require('axios');
 
 const pvsController = require('../controllers').pvs;
 const config=require("../config/configs");
@@ -25,7 +26,23 @@ router.post('/resultats', function (req, res, next) {
   
   // A ce niveau ci on doit appeler le génerateur de pvs pour qu'il fasse son travait
   // tout en stockant les résultats dans la blockchain
-  res.render('resultats',{nbrCandidat:req.body.nbrCandidat});
+
+  axios.post('http://localhost:5000/api?number_party='+req.body.nbrCandidat+'&bureau_number='+req.body.nbrBureau+'&enrolled_number='+req.body.nbrInscrit+'&coalition_mode='+req.body.typeFraude)
+  .then(function (response) {
+    if(response === "200"){
+      res.render('resultats',{nbrCandidat:req.body.nbrCandidat});
+    }
+    else if (response === "404"){
+      res.status(404).send("Une erreur est survenue. Veuillez réessayer");
+    }
+    
+  })
+  .catch(function (error) {
+      errors.push(error.response.data.errors[0].message);
+
+  })
+
+  
 });
 
 
